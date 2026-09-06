0fcfaaa1d1090d26cde10e740a132b8fba1eadeb
Cristichi <cristichi@hotmail.es>
2026-09-06 12:07:38 +0200

    Added fancy window
    To show when the API is open and be able to close it.

--- src/main/java/es/cristichi/baseapi/BaseAPIMain.java
+++ src/main/java/es/cristichi/baseapi/BaseAPIMain.java
@@ -4,28 +4,49 @@ import com.sun.net.httpserver.HttpServer;
 import es.cristichi.baseapi.rest.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
  * @author Cristichi
  */
 public class BaseAPIMain {
+    private static JFrame window = null;
     private static HttpServer server = null;
     private static boolean stopping = false;
     
-    public static void shutdown(){
+    public static void shutdown() {
         if (!stopping){
             stopping = true;
-            new Thread(() -> {
-                if (server != null){
-                   server.stop(3);
-                }
-            }).start();
+            window.dispose();
         }
     }
 
     public static void main(String[] args) {        
         try {
+            window = new JFrame("Base API");
+            window.setSize(500, 400);
+            window.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    System.out.println("Window is closing. Closing server.");
+                    if (server != null){
+                        server.stop(2);
+                    }
+                }
+            });
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            window.setLayout(new BorderLayout());
+            window.add(new JLabel("Close this window to close the API."), BorderLayout.CENTER);
+            window.add(new JLabel("UI design is my passion."), BorderLayout.SOUTH);
+            window.setVisible(true);
+
             System.out.println("Creating server...");
             server = HttpServer.create(new InetSocketAddress("localhost", 935), 0);
             
