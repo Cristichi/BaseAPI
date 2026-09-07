87ed674837c24701c0086969bf74b5351979b402
Cristichi <cristichi@hotmail.es>
2026-09-07 19:19:22 +0200

    Added method to change own data.
    For now you can only change your username. Your email is an id and your scopes are set by admins.

--- src/main/java/es/cristichi/baseapi/obj/rest/user/UserMeHandler.java
+++ src/main/java/es/cristichi/baseapi/obj/rest/user/UserMeHandler.java
@@ -3,9 +3,14 @@ package es.cristichi.baseapi.obj.rest.user;
 import com.sun.net.httpserver.HttpExchange;
 
 import es.cristichi.baseapi.obj.data.User;
+import es.cristichi.baseapi.obj.io.DataStore;
 import es.cristichi.baseapi.obj.rest.HttpHandlerAdapter;
 
 import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class UserMeHandler extends HttpHandlerAdapter {
 
@@ -16,5 +21,23 @@ public class UserMeHandler extends HttpHandlerAdapter {
     @Override
     protected HttpResponse handleGET(HttpExchange request, User requester) throws IOException {
         return HttpResponse.fromJSON(200, requester.toThemselvesSafe());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected HttpResponse handlePOST(HttpExchange request, User requester) throws IOException {
+        String body = new String(request.getRequestBody().readAllBytes());
+        try {
+            JSONObject bodyJson = (JSONObject) new JSONParser().parse(body);
+            if (bodyJson.containsKey("username")) {
+                requester.put("username", bodyJson.get("username"));
+            }
+            DataStore.getInstance().putUser(requester);
+            return HttpResponse.fromJSON(200, requester.toThemselvesSafe());
+        } catch (ParseException e) {
+            return new HttpResponse.JsonBuilder(500)
+                    .error(e)
+                    .build();
+        }
     }
 }
